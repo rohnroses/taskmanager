6973053052c366949fd73d46cfd61ae7a348fe57
rohnroses <rohnroses@example.com>
2025-10-21 15:15:14 +0500

    some settings for deploy

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-#^7d@76hx%m2tmu)@xev!uq8c3n)g&+=!9ucyd5jr-zzj31a!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['taskmanager-kjf1.onrender.com', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://taskmanager-kjf1.onrender.com']
 
 
 # Application definition
@@ -75,10 +77,11 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('postgresql://taskmanager_db_p2j0_user:DXlXPQ9dKIHk64oC2ogNxUxbKeusJib7@dpg-d3r2gpur433s73e4fiq0-a/taskmanager_db_p2j0'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
